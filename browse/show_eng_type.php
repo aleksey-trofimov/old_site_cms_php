28000d5742ef16e6bea5a053f5a6fb537404dfde
aleksey-trofimov <73225151+aleksey-trofimov@users.noreply.github.com>
2020-10-21 16:25:14 +0300

    Create show_eng_type.php

---
+++ browse/show_eng_type.php
@@ -0,0 +1,412 @@
+<HTML>
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=windows-1251">
+
+<title>Туристическая фирма Палома Турс</title>
+
+<style type="text/css">
+body{font-family:Tahoma,Helvetica,sans-serif;color:#212676;background:#A8DCF2; padding-top: 10px;}
+.text_news_header { COLOR: #525152; FONT-SIZE: 15px; FONT-STYLE: normal; font-weight: bold; FONT-FAMILY: Helvetica; TEXT-DECORATION: none;}
+.text_verdana { FONT-SIZE: 12px; FONT-STYLE: normal; FONT-FAMILY: Helvetica; TEXT-DECORATION: none}
+
+.content_table {padding-left: 9px; padding-top: 0px; padding-bottom: 0px; padding-right: 9px;}
+.menu_text { padding-left: 5px; padding-top: 3px; padding-bottom: 3px; FONT-SIZE: 15px; COLOR: #FFFFFF; font-weight: bold;}
+.menu_text1 { padding-left: 0px; padding-top: 0px; padding-bottom: 0px; FONT-SIZE: 15px; COLOR: #FFFFFF; font-weight: bold;}
+.menu_text_eng { padding-left: 5px; padding-top: 0px; padding-bottom: 0px; FONT-SIZE: 13px; COLOR: #FFFFFF; font-weight: bold;}
+.menu_text_black { padding-left: 0px; padding-top: 0px; padding-bottom: 0px; FONT-SIZE: 12px; COLOR: #000000; font-weight: bold;}
+.large_text { padding-left: 0px; padding-top: 3px; padding-bottom: 3px; FONT-SIZE: 22px; COLOR: #FFFFFF; font-weight: bold;}
+.medium_text { padding-left: 0px; padding-top: 3px; padding-bottom: 3px; FONT-SIZE: 18px; COLOR: #FFFFFF; font-weight: bold;}
+.tour_name { padding-left: 0px; padding-top: 3px; padding-bottom: 3px; FONT-SIZE: 20px; COLOR: #ED008C; font-weight: bold;}
+.tour_desc { padding-left: 0px; padding-top: 3px; padding-bottom: 3px; FONT-SIZE: 14px; COLOR: #ED008C; font-weight: bold;}
+.hot_tour_name { padding-left: 0px; padding-top: 3px; padding-bottom: 3px; FONT-SIZE: 20px; COLOR: #293094; font-weight: bold;}
+.hot_tour_desc { padding-left: 0px; padding-top: 3px; padding-bottom: 3px; FONT-SIZE: 14px; COLOR: #343DBE; font-weight: bold;}
+.eng_tour_name { padding-left: 0px; padding-top: 3px; padding-bottom: 3px; FONT-SIZE: 20px; COLOR: #E64226; font-weight: bold; text-decoration: none; COLOR: #353292}
+.eng_head { padding-left: 0px; padding-top: 3px; padding-bottom: 3px; FONT-SIZE: 25px; COLOR: #E64226; font-weight: bold;}
+.eng_tour_desc { padding-left: 0px; padding-top: 3px; padding-bottom: 3px; FONT-SIZE: 12px; COLOR: #E64226; font-weight: bold;}
+.eng_desc { padding-left: 0px; padding-top: 3px; padding-bottom: 3px; FONT-SIZE: 12px; COLOR: #E64226; font-weight: normal; COLOR: #493B92}
+.tours_down_menu_text { padding-left: 7px; padding-top: 3px; padding-bottom: 3px; padding-right: 7px; FONT-SIZE: 12px; COLOR: #ED008C; font-weight: normal;}
+.hot_down_menu_text { padding-left: 7px; padding-top: 3px; padding-bottom: 3px; padding-right: 7px; FONT-SIZE: 12px; COLOR: #00703B; font-weight: normal;}
+.eng_down_menu_text { padding-left: 7px; padding-top: 3px; padding-bottom: 3px; padding-right: 7px; FONT-SIZE: 12px; COLOR: #493B92; font-weight: normal;}
+.text_menu { COLOR: #000000; FONT-SIZE: 13px; FONT-STYLE: normal; font-weight: bold; TEXT-DECORATION: none;}
+.text_red_header { COLOR: #FF0000; FONT-SIZE: 13px; FONT-STYLE: normal; font-weight: bold; TEXT-DECORATION: none;}
+.text_red_small_header { COLOR: #FF0000; FONT-SIZE: 11px; FONT-STYLE: normal; font-weight: bold; TEXT-DECORATION: none;}
+.text_news { COLOR: #000000; FONT-SIZE: 11px; FONT-STYLE: normal; font-weight: normal; TEXT-DECORATION: none;}
+.text_main_content { COLOR: #000000; FONT-SIZE: 12px; FONT-STYLE: normal; font-weight: normal; TEXT-DECORATION: none;}
+.text_copyright { COLOR: #000000; FONT-SIZE: 10px; FONT-STYLE: normal; font-weight: normal; TEXT-DECORATION: none;}
+A.menu_link { COLOR: #ffffff; TEXT-DECORATION: none; FONT-SIZE: 18px;}
+A { COLOR: #493B92; TEXT-DECORATION: underline}
+
+
+
+
+/*Стили для отображения меню*/
+ul.mapps { font-size:11pt; font-weight:bold; margin: 0px; padding: 0px; width: 100%; list-style: none; background: #ffffff;}
+ul li ul li.mapps { font-size:10pt; font-weight:bold; padding-bottom: 5px; margin: 0px; padding-left: 5px; TEXT-DECORATION: none; background: #FFFFFF;  }
+
+ul li a.mapps_text { vertical-align: top; width: 100%; display: block; padding-left: 6px;}
+ul li ul li a.mapps_text { width: 100%; display: block; padding: 0px; }
+
+  
+/*Раскрашиваем буковки */
+ul li a:hover.mapps_text { color: #ffffff; background: #cccccc;}
+ul li a:visited.mapps_text { color: #ffffff;}
+ul li a:active.mapps_text { color: #ffffff; background: #4975C1;}
+ul li a:link.mapps_text { color: #ffffff; background: #4975C1; }
+
+ul li ul li a:hover.mapps_text { color: #6633cc; background: #cccccc; }
+ul li ul li a:visited.mapps_text { color: #6633cc; }
+ul li ul li a:active.mapps_text { color: #6633cc; background: #4975C1; }
+ul li ul li a:link.mapps_text { color: #6633cc; background: #FFFFFF; }
+
+</style>
+</head>
+<body leftmargin="0" rightmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<center>
+
+<table width="850" height="500" border=0 cellpadding=0 cellspacing=0 bgcolor="#A8DCF2">
+ <tr>
+
+ <td width="260" valign="top" border="0">
+<a href="http://paloma.ellink.ru">
+  <img src="/common/img/eng_logo.jpg" width="260" height="72" border="0" style="display:block; margin:0;">
+</a>
+<!--  <img src="/common/img/tours_logo.jpg" width="260" height="84" border="0" style="display:block; margin:0;">-->
+   <table width="260" height="470" border=0 cellpadding=0 cellspacing=0 bgcolor="#00ADEE">
+  <tr>   
+   <td width="5" border="0" background="/common/img/eng_border_left.jpg"></td>
+   <td width="250" border="0" valign="top" class="menu_text_eng" style="background: url(../common/img/eng_gradient.jpg) repeat-x;">
+<a href="http://paloma.ellink.ru/browse/show_eng_type.php?var_id=243" style="text-decoration: none;">
+<font class="large_text">Welcome to Pskov!</font>
+</a>
+<br>&nbsp;&nbsp;&nbsp;Services for foreign visitors<br><br>
+
+<a href="http://paloma.ellink.ru/browse/show_eng_type.php?var_id=245" style="text-decoration: none;"><font class="medium_text"><font class="medium_text">Ten reasons to visit Pskov </font></a>
+<br>
+<a href="http://paloma.ellink.ru/browse/show_eng_type.php?var_id=246" style="text-decoration: none;"><font class="medium_text"><font class="medium_text">Upcoming Events</font></a>
+<br>
+
+<a href="http://paloma.ellink.ru/browse/show_eng_type.php?var_id=230" style="text-decoration: none;"><font class="medium_text"><font class="medium_text">Paloma Tours</font></a>
+<br>
+<a href="http://paloma.ellink.ru/browse/show_eng_type.php?var_id=231" style="text-decoration: none;"><font class="medium_text">A word from our director</font></a>
+<br>
+
+<div style="padding-left: 0px;">
+<?php
+ $server_path = "/home/vir/paloma";
+ $db = mysql_connect("localhost", " ", " ");
+ mysql_select_db("paloma",$db);
+
+ $variants_list = mysql_query("SELECT id, name FROM records WHERE parent_id=232 ORDER BY priority",$db)
+   or die("Invalid query: " . mysql_error());
+
+//Отображаем список вариантов туров
+
+ if( $variants_list )
+ {
+  while ( $variant = mysql_fetch_array($variants_list) )
+  {
+   $variant_name = $variant["name"];
+   $variant_id = $variant["id"];
+   echo("<a class=\"menu_link\" href=\"http://paloma.ellink.ru/browse/show_eng_type.php?var_id=$variant_id\">$variant_name</a>");
+   echo("<br>");
+  }
+ }
+
+?>
+</div>
+<a href="http://paloma.ellink.ru/browse/show_eng_type.php?var_id=234" style="text-decoration: none;"><font class="medium_text"><font class="medium_text">Places to visit</font></a>
+<div style="padding-left: 0px;">
+<?php
+/* $db = mysql_connect("localhost", " ", " ");
+ mysql_select_db("paloma",$db);
+
+ $variants_list = mysql_query("SELECT id, name FROM records WHERE parent_id=234 ORDER BY priority",$db)
+   or die("Invalid query: " . mysql_error());
+
+//Отображаем список вариантов туров
+
+ if( $variants_list )
+ {
+  while ( $variant = mysql_fetch_array($variants_list) )
+  {
+   $variant_name = $variant["name"];
+   $variant_id = $variant["id"];
+   echo("<a class=\"menu_link\" href=\"http://paloma.ellink.ru/browse/show_eng_type.php?var_id=$variant_id\">$variant_name</a>");
+   echo("<br>");
+  }
+ }
+*/
+?>
+</div>
+
+<a href="http://paloma.ellink.ru/browse/show_eng_type.php?var_id=247" style="text-decoration: none;"><font class="medium_text">Testimonials</font></a>
+<br>
+
+
+
+<div style="height: 10px;"><!-- --></div>
+<img src="/common/img/eng_pic.jpg" /><br>
+<div style="height: 10px;"><!-- --></div>
+     </td>
+     <td width="5" border="0" background="/common/img/eng_border_right.jpg"></td>
+    </tr>
+   </table>
+  <img src="/common/img/eng_bottom.jpg" width="260">
+<!--////////////////////////////////////////////////////////////////////////////// -->
+<div style="height: 10px;"></div>
+
+  <img class="system" src="/common/img/en_contacts_up.jpg" width="261" height="15" border="0" style="display:block; margin:0;">
+   <table width="260" height="50" border=0 cellpadding=0 cellspacing=0 bgcolor="#00ADEE">
+    <tr>   
+     <td width="5" border="0" background="/common/img/eng_border_left.jpg"></td>
+     <td width="250" border="0" valign="top" class="menu_text" bgcolor="#00ADEE">
+<?php
+ $db = mysql_connect("localhost", " ", " ");
+ mysql_select_db("paloma",$db);
+
+
+ $contacts = mysql_query("SELECT content_en FROM records WHERE id=103",$db)
+   or die("Invalid query: " . mysql_error());
+
+ $_contacts = mysql_fetch_array($contacts);
+ $contacts_content = $_contacts["content_en"];
+
+ echo($contacts_content);
+?>
+
+</td>
+     <td width="5" border="0" background="/common/img/eng_border_right.jpg"></td>
+    </tr>
+   </table>
+  <img class="system" src="/common/img/en_contacts_bottom.jpg" width="260">
+
+<!--///////////////////////////////////////////////////////////////////////////////// -->
+
+<!--////////////////////////////////////////////////////////////////////////////// -->
+<div style="height: 10px;"></div>
+
+  <img class="system" src="/common/img/en_contacts_up.jpg" width="261" height="15" border="0" style="display:block; margin:0;">
+   <table width="260" height="50" border=0 cellpadding=0 cellspacing=0 bgcolor="#00ADEE">
+    <tr>   
+     <td width="5" border="0" background="/common/img/eng_border_left.jpg"></td>
+     <td width="250" border="0" valign="top" class="menu_text" bgcolor="#00ADEE">
+
+Our partners:<br><br>
+<a href="http://www.childreninmoscow.ru">Children in Moscow</a>
+</td>
+     <td width="5" border="0" background="/common/img/eng_border_right.jpg"></td>
+    </tr>
+   </table>
+  <img class="system" src="/common/img/en_contacts_bottom.jpg" width="260">
+
+<!--///////////////////////////////////////////////////////////////////////////////// -->
+
+
+ </td>
+ <td width="10"></td>
+
+ <td valign="top">
+
+
+<?php
+//На входе имеем идентификатор. Отображаем его контент и потомков.
+//Вычисляем путь к картинкам.
+
+if($var_id)
+{
+ $var_query = mysql_query("SELECT id, parent_id, name, content, PHP_MENU_NAME FROM records WHERE id=$var_id",$db)
+ or die("Invalid query: " . mysql_error());
+
+ $var_data = mysql_fetch_array($var_query);
+ $var_data_name = $var_data["PHP_MENU_NAME"];
+ $var_data_content = $var_data["content"];
+ $var_data_parent = $var_data["parent_id"];
+ $var_web_path = "";
+
+ //Строим путь к каталогу записи
+ if($var_data_parent != 225)
+ {
+  //Имеем как минимум второй уровень вложенности, собираем информацию о родителе записи.
+  $var_query = mysql_query("SELECT id, parent_id FROM records WHERE id=$var_data_parent",$db)
+   or die("Invalid query: " . mysql_error());
+
+  $var_parent1 = mysql_fetch_array($var_query);
+  $var_parent1_parent = $var_parent1["parent_id"];   
+
+  if($var_parent1_parent != 225)
+  {
+   //Имеем как минимум третий уровень вложенности, собираем информацию о родителе записи.
+   $var_query = mysql_query("SELECT id, parent_id FROM records WHERE id=$var_parent1_parent",$db)
+    or die("Invalid query: " . mysql_error());
+
+   $var_parent2 = mysql_fetch_array($var_query);
+   $var_parent2_parent = $var_parent2["parent_id"];
+
+   if($var_parent2_parent != 225)
+   {
+    //Имеем четвертый уровень вложенности, собираем информацию о родителе записи.
+    $var_query = mysql_query("SELECT id, parent_id FROM records WHERE id=$var_parent2_parent",$db)
+     or die("Invalid query: " . mysql_error());
+
+    $var_parent3 = mysql_fetch_array($var_query);
+    $var_parent3_parent = $var_parent3["parent_id"];
+
+    $var_web_path = "/225/$var_parent2_parent/$var_parent1_parent/$var_data_parent/$var_id"; 
+   }
+   else
+   {
+    $var_web_path = "/225/$var_parent1_parent/$var_data_parent/$var_id"; 
+   }
+  }
+  else
+  {
+   $var_web_path = "/225/$var_data_parent/$var_id"; 
+  }
+ }
+ else
+ {
+  $var_web_path = "/225/$var_id";
+ }
+
+////////////////////////////////////Отображаем заголовок, картинки и контент
+
+ echo("<div class=\"hot_tour_name\">$var_data_name</div>");
+/*
+ $droot = opendir("$server_path/$var_web_path");
+ while($root_entry = readdir($droot)) 
+ { 
+  if( $root_entry != '.' &&
+      $root_entry != '..')
+  {
+   if( !is_dir("$server_path/$var_web_path/$root_entry") )
+   {
+    $file_extension = substr($root_entry, -10); 
+    $vid_ext = substr($root_entry, -3); 
+    if( $file_extension == "_small.jpg")
+    {
+     //Нашли картинку
+     echo("<img src=\"$var_web_path/$root_entry\" height=\"127\">&nbsp;&nbsp;&nbsp;");
+    }
+   }
+  }
+ }*/
+
+// echo("<font style=\"FONT-SIZE: 12px; font-weight: bold;\">$var_data_content</font>");
+ echo("$var_data_content");
+
+//Отображаем потомков///////////////////////////////////////////////////////
+
+//Собираем информацию о потомках вызванной записи.
+ $var_query = mysql_query("SELECT id, name, content, PHP_MENU_NAME FROM records WHERE parent_id=$var_id ORDER BY priority",$db)
+  or die("Invalid query: " . mysql_error());
+
+ while( $var_child = mysql_fetch_array($var_query) )
+ {
+  $var_child_name = $var_child["PHP_MENU_NAME"];
+  $var_child_announce = $var_child["name"];
+  $var_child_id = $var_child["id"];
+
+?>
+  <table width="100%" border=0 cellpadding=0 cellspacing=0>
+   <tr><td width="100%" border="0" class="tour_name">
+        <?php echo("<a class=\"hot_tour_name\" href=\"http://paloma.ellink.ru/browse/show_eng_type.php?var_id=$var_child_id\">$var_child_name</a>");?></td></tr>
+   <tr>
+    <td width="100%" border="0">
+<?php
+  $counter = 0;
+  $droot = opendir("$server_path/$var_web_path/$var_child_id");
+  while( ($root_entry = readdir($droot)) && ($counter < 3)) 
+  {
+   if( $root_entry != '.' &&
+       $root_entry != '..')
+   {
+    if( !is_dir("$server_path/$var_web_path/$var_child_id/$root_entry") )
+    {
+     $file_extension = substr($root_entry, -10); 
+     $vid_ext = substr($root_entry, -3); 
+     if( $file_extension == "_small.jpg")
+     {
+      //Нашли картинку
+      echo("<img src=\"$var_web_path/$var_child_id/$root_entry\" height=\"127\">&nbsp;&nbsp;&nbsp;");
+      $counter++;
+     }
+    }
+   }
+  }
+?>
+    </td>
+   </tr>
+   <tr><td width="100%" border="0" class="hot_tour_desc">
+<?php echo("$var_child_announce");?>
+</tr>
+  </table>
+
+<div style="height: 10px;"></div>
+
+<?php
+ }
+}
+?>
+
+
+
+
+
+
+  </td>
+ </tr>
+</table>
+<div style="height: 12px;"></div>
+<table border=0 cellpadding=0 cellspacing=0 bgcolor="#A8DCF2">
+ <tr>
+  <td class="eng_down_menu_text" align="center">
+<a href="http://paloma.ellink.ru/browse/show_tours_type.php?var_id=151">
+ВЫБОР ТУРА
+</a>
+</td>
+  <td width="2"><img src="/common/img/down_menu_splitter.gif"></td>
+  <td class="eng_down_menu_text" align="center">
+<a href="http://paloma.ellink.ru/browse/show_tours_type.php?count_id=101">
+СТРАНЫ
+</a></td>
+  <td width="2"><img src="/common/img/down_menu_splitter.gif"></td>
+  <td class="eng_down_menu_text" align="center">
+<a href="http://paloma.ellink.ru/browse/show_hot_type.php?var_id=164">
+Горячие ПРЕДЛОЖЕНИЯ
+</a>
+</td>
+  <td width="2"><img src="/common/img/down_menu_splitter.gif"></td>
+  <td class="eng_down_menu_text" align="center">
+<a href="http://paloma.ellink.ru/browse/show_pskov_type.php?var_id=6">
+Добро пожаловать в Псковский регион
+</a>
+</td>
+  <td width="2"><img src="/common/img/down_menu_splitter.gif"></td>
+  <td class="eng_down_menu_text" align="center">
+<a href="http://paloma.ellink.ru/browse/show_eng_type.php?var_id=243">
+Welcome to Pskov
+</a>
+</td>
+ </tr>
+</table>
+</center>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-7057359-11");
+pageTracker._trackPageview();
+} catch(err) {}</script><!--LiveInternet counter--><script type="text/javascript"><!--
+document.write("<a href='http://www.liveinternet.ru/click' "+
+"target=_blank><img src='//counter.yadro.ru/hit?t44.1;r"+
+escape(document.referrer)+((typeof(screen)=="undefined")?"":
+";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+";h"+escape(document.title.substring(0,80))+";"+Math.random()+
+"' alt='' title='LiveInternet' "+
+"border='0' width='31' height='31'><\/a>")
+//--></script><!--/LiveInternet-->
+</body>
+</html>
